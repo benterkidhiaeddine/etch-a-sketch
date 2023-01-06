de5b00cf54132bc54ba87dc16cc938cea596c1dd
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-06 16:03:58 +0100

    Add functionality for erasing the drawing

--- index.js
+++ index.js
@@ -3,12 +3,66 @@
 //modify this to get change the  default number of pixels used for the sketching board
 let numberOfPixels = 16;
 
+//modify the drawing color
+let color ="black";
+
+//flag to keep track if we are in drawing mode or not
+let drawMode=false;
+
 //select the sketch container 
 const sketchContainer = document.getElementById(("sketch-container"));
 
+//def function to add the functionality of the drawboard that takes color as a paramater
+function functionality(color){
+    //add the functionality to the draw board
+    //list of cells 
+    const cells = document.querySelectorAll(".cell");
+
+    
+
+    sketchContainer.addEventListener("mousedown",function(){
+        drawMode=true;
+    });
+    sketchContainer.addEventListener("mouseup",function(){
+        drawMode=false;
+    })
+
+    //functionality when holding and moving the mouse button it will draw
+    for (element of cells){
+        element.addEventListener("mousemove",function(e){
+            if (drawMode){
+                e.target.style.backgroundColor=`${color}`;
+            }
+        
+        })
+    }
+
+}
+
+//function for validating the input
+function validateInput(){
+    numberOfPixels = parseInt(prompt("PLeas enter the number of wanted pixels on the sketch board"));
+    
+    if(isNaN(numberOfPixels)){
+        alert("Pleas enter a valid number not greater than 100");
+        validateInput();
+    }
+
+    if(numberOfPixels>100){
+        numberOfPixels = 16;
+        alert("The number of pixels was set to 16 pleas refrain from entering a number bigger than 100");
+        validateInput();
+
+    }
+    else if(numberOfPixels<=0){
+        numberOfPixels=16;
+        alert("The number of pixels was set to 16 pleas refrain from entering a negative number or zero number");
+        validateInput();
+    }
+    }
 
 //make the drawing of the sketch board into a function
-function drawSketchBoard(numberOfPixels){
+function drawSketchBoard(numberOfPixels,color){
     //reset the draw board
     sketchContainer.innerHTML="";
     for (let i=0;i<numberOfPixels;i++){
@@ -33,67 +87,29 @@ function drawSketchBoard(numberOfPixels){
         sketchContainer.appendChild(row);
         
     }
-    //add the functionality to the draw board
-    //list of cells 
-    const cells = document.querySelectorAll(".cell");
+    functionality(color);
 
-    //flag to keep track if we are in drawing mode or not
-    let drawMode=false;
-
-    sketchContainer.addEventListener("mousedown",function(){
-        drawMode=true;
-    });
-    sketchContainer.addEventListener("mouseup",function(){
-        drawMode=false;
-    })
-
-    //functionality when holding and moving the mouse button it will draw
-    for (element of cells){
-        element.addEventListener("mousemove",function(e){
-            if (drawMode){
-                e.target.style.backgroundColor="black";
-            }
-        
-        })
-    }
 
 
     }
 
-//draw the default sketchBoard
-drawSketchBoard(numberOfPixels);
+//draw the default sketchBoard with default numberOfPixels and default color
+drawSketchBoard(numberOfPixels,color);
 
 
-//function for validating the input
-function validateInput(){
-    numberOfPixels = parseInt(prompt("PLeas enter the number of wanted pixels on the sketch board"));
-    
-    if(isNaN(numberOfPixels)){
-        alert("Pleas enter a valid number not greater than 100");
-        validateInput();
-    }
 
-    if(numberOfPixels>100){
-        numberOfPixels = 16;
-        alert("The number of pixels was set to 16 pleas refrain from entering a number bigger than 100");
-        validateInput();
-
-    }
-    else if(numberOfPixels<=0){
-        numberOfPixels=16;
-        alert("The number of pixels was set to 16 pleas refrain from entering a negative number or zero number");
-        validateInput();
-    }
-    }
 //button for choosing number of pixels
 const pixelNumberButton=document.getElementById("pixelNumber");
 pixelNumberButton.addEventListener("click",function(){
     validateInput();
     
-    drawSketchBoard(numberOfPixels);
+    drawSketchBoard(numberOfPixels,color);
    
 })
 
 
+//button for erasing 
 
+const erase =document.getElementById("eraser");
+erase.addEventListener("click",()=>functionality("white"));
 
