98d1238b99cda34e5a021dbdea9204f0a5627bed
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-06 14:18:28 +0100

    Made the  sketch container hold a varying number of pixels

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 
 //modify this to get change the number of pixels used for the sketching board
-const rows = 16;
-const columns = 16;
+let numberOfPixels = 64;
 
 //select the sketch container 
 
 const sketchContainer = document.getElementById(("sketch-container"));
 //create a loop to add divs that represent rows
-for (let i=0;i<rows;i++){
+for (let i=0;i<numberOfPixels;i++){
     //create a  row div
     const row=document.createElement("div");
     //add a class row to the div so it can be styled
     row.classList.add("row");
+    //make the height of the row modifiable according to the number of pixels 
+    row.style.height=`calc(100%/${numberOfPixels})`;
     //create a loop to add  16 cells for each row
-    for(let i=0;i<columns;i++){
+    for(let i=0;i<numberOfPixels;i++){
         //create a cell div
         const cell =document.createElement("div");
         //add a cell class to the div so it can be styled
         cell.classList.add("cell");
+        //make the width of the cell modifiable according to the number of pixels 
+        cell.style.width=`calc(100%/${numberOfPixels})`;
         //add the cell to the row div
         row.appendChild(cell);
     }
